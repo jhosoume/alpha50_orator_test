83d26a4bd8b71df5f133389c31df67854183e94a
Juliana Hosoume <ju.hosoume@gmail.com>
2016-07-09 18:48:42 -0700

    Add name column to stocks

---
+++ db/migrations/2016_07_10_013730_add_name_to_stocks_table.py
@@ -0,0 +1,18 @@
+from orator.migrations import Migration
+
+
+class AddNameToStocksTable(Migration):
+
+    def up(self):
+        """
+        Run the migrations.
+        """
+        with self.schema.table('stocks') as table:
+            table.string('name')
+
+    def down(self):
+        """
+        Revert the migrations.
+        """
+        with self.schema.table('stocks') as table:
+            table.drop_columen('name')
